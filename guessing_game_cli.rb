b15a890bd035f6f0276c9a0eac699913149399c2
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 18:36:17 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,11 @@
 # Code your solution here!
 require 'pry'
 def run_guessing_game
-  binding.pry
+  #binding.pry
   number = rand(6)+1
   #puts "Pick a number between 1 and 6"
   answer = gets.chomp
+  puts answer
   #binding.pry
   if answer == number
     puts "You guessed the correct number!".chomp
