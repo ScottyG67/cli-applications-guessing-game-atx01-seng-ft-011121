3696f24ba4186a5f40b9bbf27db62ed8828a035f
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 18:40:13 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,9 @@
 def run_guessing_game
 
   number = (rand(6)+1).to_s
-  #answer = gets.chomp
-  answer = get_input
+  puts "Pick a number between 1 and 6.".chomp
+  answer = gets.chomp
+  
   if answer == number
     puts "You guessed the correct number!".chomp
   elsif answer == "exit"
